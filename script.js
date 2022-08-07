90fdc1802e0dc4bb1eb9661e75eb2395aa60b703
msvl <masha.pav.nov1@gmail.com>
2022-08-07 11:45:25 +0300

    more-link

---
+++ script.js
@@ -0,0 +1,6 @@
+function readMore(){
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btn = document.getElementById("more-link-id");
+    
+}
