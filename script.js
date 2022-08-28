2a40ea5c35273798b32af1618b231aecc850a6ba
msvl <masha.pav.nov1@gmail.com>
2022-08-28 11:01:07 +0300

    логика кнопок

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function readMore(){
-    let dots = document.getElementById("dots");
-    let moreText = document.getElementById("more");
-    let btn = document.getElementById("more-link-id");
-    
+function readMoreSpring(){
+    let dots = document.getElementById("dotsSpring");
+    let moreText = document.getElementById("moreTextSpring");
+    let btn = document.getElementById("btnSpring");
+
+    if (dots.style.display === 'none'){
+        dots.style.display = 'inline';
+        moreText.style.display = 'none';
+        btn.innerHTML = 'Читать больше';
+    } else {
+        dots.style.display = 'none';
+        moreText.style.display = 'inline';
+        btn.innerHTML = 'Читать меньше';
+    }
+}
+
+
+function readMoreWinter(){
+    let dots = document.getElementById("dotsWinter");
+    let moreText = document.getElementById("moreTextWinter");
+    let btn = document.getElementById("btnWinter");
+
+    if (dots.style.display === 'none'){
+        dots.style.display = 'inline';
+        moreText.style.display = 'none';
+        btn.innerHTML = 'Читать больше';
+    } else {
+        dots.style.display = 'none';
+        moreText.style.display = 'inline';
+        btn.innerHTML = 'Читать меньше';
+    }
 }
